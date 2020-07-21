8438d18314ca107075bfbaf14587a91a7f7abfe6
Deepak Ramrakhyani <dpkramrakhyani@gmail.com>
2020-07-21 19:46:30 +0530

    Removed animations library

--- lib/route/route_generator.dart
+++ lib/route/route_generator.dart
@@ -1,4 +1,3 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:mental_health/route/route_name.dart';
 import 'package:mental_health/view/pages/forgot_password_page.dart';
@@ -20,60 +19,18 @@ class RouteGenerator {
       case RouteName.INITIAL:
         return MaterialPageRoute(builder: (_) => WelcomePage());
       case RouteName.LOGIN:
-        return fadeThrough(LoginPage());
+        return MaterialPageRoute(builder: (_) => LoginPage());
       case RouteName.REGISTER:
-        return fadeThrough(RegisterPage());
+        return MaterialPageRoute(builder: (_) => RegisterPage());
       case RouteName.HOME:
-        return fadeThrough(HomePage());
+        return MaterialPageRoute(builder: (_) => HomePage());
       case RouteName.FORGOT_PASSWORD:
-        return fadeThrough(ForgotPasswordPage());
+        return MaterialPageRoute(builder: (_) => ForgotPasswordPage());
 
       default:
         // If there is no such named route in the switch statement, e.g. /third
         return _errorRoute();
     }
-  }
-
-  static Route<T> fadeThrough<T>(Widget page,
-      [double duration = kDefaultDuration]) {
-    return PageRouteBuilder<T>(
-      transitionDuration: Duration(milliseconds: (duration * 1000).round()),
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return FadeThroughTransition(
-            animation: animation,
-            secondaryAnimation: secondaryAnimation,
-            child: child);
-      },
-    );
-  }
-
-  static Route<T> fadeScale<T>(Widget page,
-      [double duration = kDefaultDuration]) {
-    return PageRouteBuilder<T>(
-      transitionDuration: Duration(milliseconds: (duration * 1000).round()),
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return FadeScaleTransition(animation: animation, child: child);
-      },
-    );
-  }
-
-  static Route<T> sharedAxis<T>(Widget page,
-      [SharedAxisTransitionType type = SharedAxisTransitionType.scaled,
-      double duration = kDefaultDuration]) {
-    return PageRouteBuilder<T>(
-      transitionDuration: Duration(milliseconds: (duration * 1000).round()),
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return SharedAxisTransition(
-          child: child,
-          animation: animation,
-          secondaryAnimation: secondaryAnimation,
-          transitionType: type,
-        );
-      },
-    );
   }
 
   static Route<dynamic> _errorRoute() {

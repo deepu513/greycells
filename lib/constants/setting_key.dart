c07ab771282ea6035c3b51e7a2a916976796f6bc
Deepak Ramrakhyani <dpkramrakhyani@gmail.com>
2020-09-19 21:15:41 +0530

    Removed unused keys

--- lib/constants/setting_key.dart
+++ lib/constants/setting_key.dart
@@ -13,9 +13,4 @@ class SettingKey {
   static const KEY_USER_MOBILE = const SettingKey._internal("mobile_number");
   static const KEY_REQUEST_TOKEN = const SettingKey._internal("request_token");
   static const KEY_REFRESH_TOKEN = const SettingKey._internal("refresh_token");
-  static const KEY_PATIENT_DETAIL_SUBMITTED =
-      const SettingKey._internal("patient_detail_submitted");
-  static const KEY_FIRST_TEST_DONE = const SettingKey._internal("first_test_done");
-  static const KEY_SECOND_TEST_DONE = const SettingKey._internal("second_test_done");
-  static const KEY_CURRENT_QUESTION = SettingKey._internal("current_question");
 }

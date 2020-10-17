7d03dffc683a401a18d97aff6b271955807d8d32
Deepak Ramrakhyani <dpkramrakhyani@gmail.com>
2020-10-17 09:32:25 +0530

    Improved UI of header card

--- lib/view/pages/patient_home_page.dart
+++ lib/view/pages/patient_home_page.dart
@@ -69,56 +69,68 @@ class HeaderCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: InkWell(
-        onTap: () {},
-        borderRadius: BorderRadius.circular(16.0),
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16.0),
-            gradient: LinearGradient(
-              colors: [Color(0xFFC984A1), Color(0xFF4D2294)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+    return Stack(
+      children: [
+        Card(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.0),
+              gradient: LinearGradient(
+                colors: [Color(0xFFC984A1), Color(0xFF4D2294)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  "images/self_care_illustration.svg",
+                  height: 80.0,
+                  width: 80.0,
+                ),
+                SizedBox(
+                  width: 8.0,
+                ),
+                Expanded(
+                  child: Text(
+                    "Checkout your assessment score",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle1
+                        .copyWith(color: Colors.white),
+                    overflow: TextOverflow.clip,
+                  ),
+                ),
+                SizedBox(
+                  width: 8.0,
+                ),
+                Icon(
+                  Icons.chevron_right,
+                  color: Colors.white,
+                )
+              ],
             ),
           ),
-          child: Row(
-            children: [
-              SvgPicture.asset(
-                "images/self_care_illustration.svg",
-                height: 80.0,
-                width: 80.0,
-              ),
-              SizedBox(
-                width: 8.0,
-              ),
-              Expanded(
-                child: Text(
-                  "Checkout your assessment score",
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle1
-                      .copyWith(color: Colors.white),
-                  overflow: TextOverflow.clip,
-                ),
-              ),
-              SizedBox(
-                width: 8.0,
-              ),
-              Icon(
-                Icons.chevron_right,
-                color: Colors.white,
-              )
-            ],
-          ),
+          elevation: 4.0,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
         ),
-      ),
-      elevation: 4.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
-      ),
+        Positioned.fill(
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () {},
+                borderRadius: BorderRadius.circular(16.0),
+                splashColor: Colors.white24,
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
